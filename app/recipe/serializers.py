5caeaec09b8466d3e8f75a12ff636a350e6985df
Tomasz Zięba <tomasz.zieba@apptimia.com>
2021-03-10 13:19:26 +0100

    Added custo recipe detail view

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -41,3 +41,9 @@ class RecipeSerializer(serializers.ModelSerializer):
             'tags'
         )
         read_only_fields = ('id',)
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a recipe detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
+
